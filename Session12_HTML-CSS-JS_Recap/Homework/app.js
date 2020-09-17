c32a7980e498e542ef14d46f57c3172a7fc36196
Valentin Vandra <vandravalentin@gmail.com>
2020-09-17 15:33:37 +0300

    adaugare obiect

--- Session12_HTML-CSS-JS_Recap/Homework/app.js
+++ Session12_HTML-CSS-JS_Recap/Homework/app.js
@@ -1,85 +1,50 @@
-let addItem = document.getElementById('elAdd');
-let elements = [];
-let elList = document.querySelector('.elList');
+let addItem = document.getElementById('elAdd'); //selectam butonul de 'Add Item'
+const elList = document.querySelector('.elList'); //selectam div-ul in care urmeaza sa fie inserat tabelul
+const newTable = document.createElement('table'); //creem un element table
+elList.appendChild(newTable); //adaugam tabelul nou creat in div
 
-let newTable = document.createElement('table');
-elList.appendChild(newTable);
+
+class Database {
+    constructor(items = [], markElem = []) {
+        this.items = items;
+        this.markElem = markElem;
+    }
+
+
+}
+
+shoppingList = new Database();
 
 addItem.addEventListener('click', e => {
-    let element = document.getElementById('element').value;
-    // let elList = document.querySelector('.elList');
+    let element = document.getElementById('element').value; //luam valuarea din iput la fiecare apasare a butonului
 
-    // let newTable = document.createElement('table');
-
-    // let newTr = document.createElement('tr');
-    // let newTd1 = document.createElement('td');
-    // let newTd2 = document.createElement('td');
-    // newTd1.className = 'elList-control';
-    // newTd2.className = 'elList-control';
-
-    // console.log(elements);
-
-
-
-
-    if (elements == "") {
-        elements.push(element);
+    if (shoppingList.items == "") {
 
         let newTr = newTable.insertRow(0);
         let newTd1 = newTr.insertCell(0);
         let newTd2 = newTr.insertCell(1);
+
         newTd1.className = 'elList-control';
         newTd2.className = 'elList-control';
+
         newTd1.innerHTML = '<strong> Item description </strong>';
         newTd2.innerHTML = '<strong> Action </strong>';
 
 
     }
 
-    elements.push(element);
-
     let newTr = newTable.insertRow(1);
     let newTd1 = newTr.insertCell(0);
     let newTd2 = newTr.insertCell(1);
-    newTd1.className = 'elList-control';
+
+    newTd1.className = 'elList-control item';
     newTd2.className = 'elList-control';
 
     newTd1.innerHTML = element;
     newTd2.innerHTML = '<button class="mark">Mark as buyed</button>';
 
+    shoppingList.items.push(newTd1);
+    shoppingList.markElem.push(newTd2.innerHTML);
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var newLi = document.createElement('li'); //am creat un nou li
-
-// newLi.className = 'collection-item'; //ii dam o clasa si un id
-// newLi.id = 'mySpecialLi';
-
-// var ulCollection = document.querySelector('.collection'); //selectam ul
-// var textNode = document.createTextNode('Eat') //facem o variabial care tine un TextNode
-// newLi.appendChild(textNode); //adaugam la li textul
-
-// var link = document.createElement('a'); //facem un nou a care are steluta de delete
-// link.className = 'delete-item secondary-content'; //dam o clasa pentru a
-
-// link.innerHTML = '<i class="fa fa-remove"></i>'; //punem iconita de delete pentru a
-
-// ulCollection.appendChild(newLi); //adaugam li-ul creat in ul
-// newLi.appendChild(link); //adaugam a-ul care contine iconita de delete in li-ul cread
-
-
-// console.log(newLi);
