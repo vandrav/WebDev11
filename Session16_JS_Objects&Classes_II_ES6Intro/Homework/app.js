997d0d5bb754e0d7c32060dbb33549d8d0b2a9e8
Valentin Vandra <vandravalentin@gmail.com>
2020-09-24 16:28:00 +0300

    fara stergere rand tabel

---
+++ Session16_JS_Objects&Classes_II_ES6Intro/Homework/app.js
@@ -0,0 +1,83 @@
+const formElem = document.getElementById('form');
+const tableElem = document.querySelector('.elList table');
+const titleElem = document.getElementById('title');
+const authorElem = document.getElementById('author');
+const isbnElem = document.getElementById('ISBN');
+
+class Book {
+    constructor(title, author, isbn, deleteBtn = '') {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    showError() {
+        let errorMessage = document.createElement('p');
+        errorMessage.className = "error";
+        errorMessage.innerHTML = "Please fill all the fields!";
+        let firstInput = document.getElementById('first-input');
+        formElem.insertBefore(errorMessage, firstInput);
+        setTimeout(() => {
+                errorMessage.remove();
+            },
+            2000);
+    }
+
+    showSuccess() {
+        let successMessage = document.createElement('p');
+        successMessage.className = "success";
+        successMessage.innerHTML = titleElem.value + " added!";
+        let firstInput = document.getElementById('first-input');
+        formElem.insertBefore(successMessage, firstInput);
+        setTimeout(() => {
+                successMessage.remove();
+            },
+            2000);
+    }
+
+    clearFields() {
+        formElem.reset();
+    }
+
+    addBook(book) {
+        let row = tableElem.insertRow();
+        let cell1 = row.insertCell();
+        let cell2 = row.insertCell();
+        let cell3 = row.insertCell();
+        let cell4 = row.insertCell();
+
+        cell1.innerHTML = book.title.value;
+        cell2.innerHTML = book.author.value;
+        cell3.innerHTML = book.isbn.value;
+        cell4.innerHTML = '<a href = "#"> X </a>';
+
+        book.deleteBtn = cell4.innerHTML;
+
+    }
+
+    deleteBook(book) {
+        let index = book.deleteBtn.parentElememnt.rowIndex;
+        tableElem.deleteRow(i);
+    }
+}
+
+
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let ui = new UI();
+
+    if (titleElem.value === '' || authorElem.value === '' || isbnElem.value === '') {
+        ui.showError();
+    } else {
+        ui.showSuccess();
+        let book = new Book(titleElem, authorElem, isbnElem);
+        ui.addBook(book);
+        book.deleteBtn.addEventListener('click', ui.deleteBook());
+
+    }
+    ui.clearFields();
+
+});
