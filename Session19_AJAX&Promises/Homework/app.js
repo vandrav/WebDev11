fc2d85b8b61653912b41b21a4d869f37ba9aa444
Valentin Vandra <vandravalentin@gmail.com>
2020-09-30 20:06:47 +0300

    Weather done - remains forecast

--- Session19_AJAX&Promises/Homework/app.js
+++ Session19_AJAX&Promises/Homework/app.js
@@ -1,10 +1,15 @@
-const input = document.getElementById('input');
+const input = document.getElementById('getLocation');
 const weatherBtn = document.getElementById('weather');
 const URL_CURRENT_WEATHER = "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
 const URL_WEATHER_ICON_PREFIX = "http://openweathermap.org/img/w/"
 
-const temp = document.getElementById('temp');
 const icon = document.getElementById('icon');
+const description = document.getElementById('desc');
+const humidity = document.getElementById('humidity');
+const pressure = document.getElementById('pressure');
+const temp = document.getElementById('temp');
+const dayMax = document.getElementById('dayMax');
+const dayMin = document.getElementById('dayMin');
 
 
 weatherBtn.addEventListener('click', showWeather);
@@ -17,8 +22,15 @@ function showWeather() {
         })
         .then(function(data) {
             console.log(data);
-            temp.innerHTML = ` ${data.main.temp} `;
             icon.src = URL_WEATHER_ICON_PREFIX + data.weather[0].icon + '.png';
+            description.innerHTML = `Descriere: ${data.weather[0].description}`;
+            humidity.innerHTML = `Umiditate: ${data.main.humidity}`;
+            pressure.innerHTML = `Umiditate: ${data.main.pressure}`;
+            temp.innerHTML = `Temperatura curenta: ${Math.round(data.main.temp)}°C`;
+            dayMin.innerHTML = `Minima zilei: ${Math.round(data.main.temp_min)}°C`;
+            dayMax.innerHTML = `Maxima zilei: ${Math.round(data.main.temp_max)}°C`;
+
+
         })
         .catch(function(err) {
             console.log(err);
