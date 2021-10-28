130a1355522ae2e6e7defc14f686413b47571525
Valentin Vandra <vandravalenti@gmail.com>
2021-10-28 16:16:14 +0300

    restructuring

--- Session12_DOM_Manipulation/Starting_code/06_removing_replacing_items/app.js
+++
@@ -1,8 +0,0 @@
-var ulCollection = document.querySelector('.collection');
-
-
-// ulCollection.removeChild(document.querySelector('li:nth-child(3)')); //am sters al-3lea element(li) din ul
-var liElements = document.getElementsByClassName('collection-item');
-ulCollection.removeChild(liElements[1]);
-
-console.log(ulCollection);
